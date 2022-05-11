934bc8c99efb8e4401ce0053723ed34d638ad5d0
Mohammad Usman <mohammadusman666@gmail.com>
2022-05-11 09:36:11 +0500

    custom annotate function

--- src/main/java/hudson/plugins/junitattachments/AttachmentTestAction.java
+++ src/main/java/hudson/plugins/junitattachments/AttachmentTestAction.java
@@ -39,12 +39,15 @@ public class AttachmentTestAction extends TestAction {
 	@Override
 	public String annotate(String text) {
 		String url = Jenkins.getActiveInstance().getRootUrl()
-				+ testObject.getRun().getUrl() + "testReport"
 				+ testObject.getUrl() + "/attachments/";
 		for (String attachment : attachments) {
-			text = text.replace(attachment, "<a href=\"" + url + attachment
+			text = text.replace('/'+attachment, "/<a href=\"" + url + attachment
 					+ "\">" + attachment + "</a>");
+			text = text.replace("taking screenshot "+attachment, "<a href=\"" + url + attachment
+					+ "\"><div style=\"position: relative\"><img width=\"100%\" src=\"" + url + attachment + "\"></img></div></a>");
 		}
+		text = text.replaceAll("</div></a>&lt;(\\d+\\.?\\d*),(\\d+\\.?\\d*)>",
+			"<div style=\"position: absolute; background-color: rgba(255,0,0,.5); height: 20px; width: 20px; border-radius: 50%; top: calc($2% - 10px); left: calc($1% - 10px)\"></div></div></a>");
 		return text;
 	}
 
